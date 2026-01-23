50775267f9a73ec93a1b3012027a4bfe4a200db1
minding2796 <minding2796@gmail.com>
2026-01-23 19:03:06 +0900

    feat: IPC v2

--- Assets/Scripts/IPC/IpcConnector.cs
+++ Assets/Scripts/IPC/IpcConnector.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Collections;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
+using System.IO;
+using System.IO.Pipes;
 using System.Threading;
 using UnityEngine;
 
@@ -10,58 +8,41 @@ namespace IPC
 {
     public class IpcConnector : MonoBehaviour
     {
-        private static Socket _server;
-        private static bool _isAlive;
-        public const int Port = 7777;
-        public static event Action<string> OnMessageReceived=_=>{};
-        
+        public static bool Opened;
+        public static event Action<string> OnMessageReceived = _ => { };
+
         private void OnEnable()
         {
-            var ipAddress = IPAddress.Parse("127.0.0.1");
-            var endPoint = new IPEndPoint(ipAddress, Port);
- 
-            _server = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-
             try
             {
-                _server.Bind(endPoint); 
-                _server.Listen(1);
-                _isAlive = true;
-
+                Opened = true;
                 new Thread(Listen).Start();
             }
             catch (Exception e)
             {
                 Debug.LogError(e.ToString());
             }
-            Debug.Log("IPC Server started: " + _server);
         }
+
+        private void OnDisable() => Opened = false;
 
         private static void Listen()
         {
-            var clientSocket = _server.Accept();
-            while (_isAlive)
+            s:
+            while (Opened)
             {
-                var receiveBuffer = new byte[1024];
-                var receiveBytes = clientSocket.Receive(receiveBuffer);
-                if (receiveBytes <= 0)
+                using var server = new NamedPipeServerStream("qss_pray_to_survive", PipeDirection.InOut);
+                if (server.IsConnected) server.Disconnect();
+                server.WaitForConnection();
+                using var reader = new StreamReader(server);
+                while (Opened)
                 {
-                    clientSocket = _server.Accept();
-                    continue;
+                    if (reader.EndOfStream) goto s;
+                    var clientMsg = reader.ReadLine();
+                    Debug.Log($"IPC Received: {clientMsg}");
+                    OnMessageReceived?.Invoke(clientMsg);
                 }
-                var receiveData = Encoding.UTF8.GetString(receiveBuffer, 0, receiveBytes);
-                OnMessageReceived.Invoke(receiveData);
-                Debug.Log($"클라이언트로부터 받음: {receiveData}");
             }
-        }
-
-        private void OnDisable()
-        {
-            _isAlive = false;
-            if (_server == null) return;
-            _server.Close();
-            _server.Dispose();
-            _server = null;
         }
     }
 }
