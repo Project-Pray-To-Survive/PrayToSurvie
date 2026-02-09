86598740ed454b07fd26326c36c1d7d363cd3aef
minding2796 <minding2796@gmail.com>
2026-02-09 09:44:05 +0900

    wip: IPC 양방향 통신

--- Assets/Scripts/IPC/IpcConnector.cs
+++ Assets/Scripts/IPC/IpcConnector.cs
@@ -10,6 +10,7 @@ namespace IPC
     {
         public static bool Opened;
         public static event Action<string> OnMessageReceived = _ => { };
+        private static StreamWriter _currentWriter;
 
         private void OnEnable()
         {
@@ -28,21 +29,34 @@ namespace IPC
 
         private static void Listen()
         {
-            s:
             while (Opened)
             {
                 using var server = new NamedPipeServerStream("qss_pray_to_survive", PipeDirection.InOut);
                 if (server.IsConnected) server.Disconnect();
                 server.WaitForConnection();
                 using var reader = new StreamReader(server);
+                using var writer = new StreamWriter(server);
+                _currentWriter = writer;
                 while (Opened)
                 {
-                    if (reader.EndOfStream) goto s;
+                    if (reader.EndOfStream) break;
                     var clientMsg = reader.ReadLine();
                     Debug.Log($"IPC Received: {clientMsg}");
                     OnMessageReceived?.Invoke(clientMsg);
                 }
+                _currentWriter = null;
             }
+        }
+
+        public void SendToPipe(string message)
+        {
+            if (_currentWriter is { BaseStream: { CanWrite: true } })
+            {
+                _currentWriter.WriteLine(message);
+                _currentWriter.Flush();
+                Debug.Log($"IPC Sent: {message}");
+            }
+            else Debug.LogWarning("No client connected to send message.");
         }
     }
 }
